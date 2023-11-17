86b1be8502a717f6f3aeb548899de9328579c5ac
lorinjacot <71598369+lorinjacot@users.noreply.github.com>
2023-11-17 11:40:34 +0100

    Fix typo in dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    Vous avez rejoins le secret santa!
+                    Vous avez rejoint le secret santa!
                 </div>
             </div>
         </div>
