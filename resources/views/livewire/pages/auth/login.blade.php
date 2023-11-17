63ba8c578a002325f87eefd3e7db241fe56b8e51
lorinjacot <71598369+lorinjacot@users.noreply.github.com>
2023-11-17 11:39:12 +0100

    lien de création de compte corrigé

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -73,8 +73,8 @@ new #[Layout('layouts.guest')] class extends Component
         </div>
         @if (Route::has('login'))
         <div class="flex items-center justify-start mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}" wire:navigate>
-                {{ __('No account ? Create one here') }}
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}" wire:navigate>
+                {{ __('No account yet ? Create one here') }}
             </a>
         </div>
         @endif
